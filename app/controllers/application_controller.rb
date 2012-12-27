8758f9dc08875eec2a6cb09ace6ece6b05150ee9
Sunday Lee <isundaylee.reg@gmail.com>
2012-12-27 14:43:04 +0800

    Implement locale setting.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
+# encoding: utf-8
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_locale
+
   include SessionsHelper
   include ApplicationHelper
+
+  LOCALES = {
+    'en' => 'English',
+    'zh' => '简体中文'
+  }
+
+  def set_locale
+    session[:locale] = params[:locale] if params[:locale]
+    I18n.locale = session[:locale] if session[:locale]
+    @flash = {}
+    @flash[:success] = I18n.t('application.set_locale.flash_success', locale_name: LOCALES[session[:locale]]) if params[:locale]
+  end
 
   private
 
